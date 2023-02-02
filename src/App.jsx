c57b44f97d0b93376f9e6497ee4d06e662b9c3d3
Emmanuel <emmysoetan@gmail.com>
2023-02-02 13:28:47 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Test from './components/test'
+
+
+function App () {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+   </div>
+
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        'poppins' : ['poppins', 'sans-serif']
+      },
+      colors : {
+        primary: '#335145',
+        secondary: '#638275',
+        accent: '#3AACFF',
+        background: '#E8F3F5'
+      }
+    },
+  },
+  plugins: [],
+}
