c4d76d44cf5eb9dc5b0a919b785744fd8ac99da0
Emmanuel <emmysoetan@gmail.com>
2023-03-11 22:27:30 +0100

    made the nav bar sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
     return (
         
-    <div className=' container flex mx-auto justify-between items-center -mt-6'>
+    <div className='sticky top-0 z-30 bg-background flex mx-auto justify-between items-center -mt-6 mb-6'>
 
         {/* restaurant Logo */}
         <div>
