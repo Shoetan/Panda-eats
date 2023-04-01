d58fc7ee7f20307d752e8d9dc2a4ab696e2bba8a
Emmanuel <emmysoetan@gmail.com>
2023-04-02 00:30:04 +0100

    finished the implementation of firebase authentication using the google sign in method

--- src/components/Dropmenu.jsx
+++ src/components/Dropmenu.jsx
@@ -1,5 +1,5 @@
 import { auth, provider } from '../firebase/firbase'
-import { signInWithPopup } from 'firebase/auth';
+import { signInWithPopup, signOut } from 'firebase/auth';
 
 
 
@@ -7,26 +7,36 @@ import { signInWithPopup } from 'firebase/auth';
 const Dropdown = () => {
 
 
-    const signIn = () =>{
+    const signInUser = () =>{
         signInWithPopup(auth, provider)
             .then((result) =>{
-                console.log(result);
+                window.localStorage.setItem('credentials',JSON.stringify(result));
             })
 
             .catch ((err) => {
-                console.log(err);
+                window.localStorage.setItem('error', err.message );
             })
+    }
+
+    const signOutUser = () => {
+       signOut(auth)
+        .then((result) => {
+            console.log(result);
+        })
+
+        .catch ((err) => {
+            console.log(err.message);
+        })
     }
 
     return ( 
         <div>
                      {/*Drop down Menu*/}
-                     <div className='border p-2 rounded-lg bg-gray-300 text-slate-700'>
-
-                        <button onClick={signIn}>
-                            login
-                        </button>
-                   
+                     <div className='border px-4 py-2 rounded-lg bg-gray-300 text-slate-700 flex items-center justify-center'>
+                        <ul>
+                            <li><a href="#"className='font-poppins text-sm hover:opacity-90'onClick={signInUser}>Login</a></li>
+                            <li><a href="#"className='font-poppins text-sm hover:opacity-90'onClick={signOutUser}>Logout</a></li>
+                        </ul>
                     </div>
 
         </div>
